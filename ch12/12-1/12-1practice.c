6281e0f0970d724602a2c4cb85aa0d35b4d2fd7a
HDLee26 <lhd2600160@kunsan.ac.kr>
2026-09-05 22:46:04 +0900

    Update 12-1practice.c

--- ch12/12-1/12-1practice.c
+++ ch12/12-1/12-1practice.c
@@ -12,9 +12,9 @@ int main(void)
 	int b = 36;
 	double c = 3.141592;
 
-	printf("char형 변수 a의 주소 : %zu \n", &a);
-	printf("int형 변수 b의 주소 : %zu \n", &b);
-	printf("double형 변수 c의 주소 : %zu \n", &c);
+	printf("char형 변수 a의 주소 : %p \n", &a);
+	printf("int형 변수 b의 주소 : %p \n", &b);
+	printf("double형 변수 c의 주소 : %p \n", &c);
 
 	return 0;
 }
