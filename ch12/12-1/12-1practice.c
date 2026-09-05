cec0ba6fc0f98d8a0cf4ad5b2919ce602b45eb69
HDLee26 <lhd2600160@kunsan.ac.kr>
2026-09-05 22:27:34 +0900

    Add files via upload

---
+++ ch12/12-1/12-1practice.c
@@ -0,0 +1,20 @@
+// *************************
+//  제목 : 주소연산자의 이용
+//  날짜 : 2026년 9월 5일
+//  작성 : 2600160 이현도
+// *************************
+
+#include <stdio.h>
+
+int main(void)
+{
+	char a = 'A';
+	int b = 36;
+	double c = 3.141592;
+
+	printf("char형 변수 a의 주소 : %zu \n", &a);
+	printf("int형 변수 b의 주소 : %zu \n", &b);
+	printf("double형 변수 c의 주소 : %zu \n", &c);
+
+	return 0;
+}
