6329745f65164eddeced1fd0b6aeb45c9a34f2b5
HDLee26 <lhd2600160@kunsan.ac.kr>
2026-09-09 22:46:42 +0900

    Add files via upload

---
+++ ch12/12-2/12-2p2.c
@@ -0,0 +1,22 @@
+﻿// *************************
+//  제목 : 간접참조연산자의 이용
+//  날짜 : 2026년 9월 9일
+//  작성 : 2600160 이현도
+// *************************
+
+#include <stdio.h>
+int main(void)
+{
+	int a = -100;
+	char b = 'A';
+	double c = 3.14;
+	int* pa = &a;
+	char* pb = &b;
+	double* pc = &c;
+
+	printf("int형 변수 a의 값은: %d\n", *pa);
+	printf("char형 변수 b의 값은: %c\n", *pb);
+	printf("double형 변수 c의 값은: %lf\n", *pc);
+	 
+	return 0;
+}
