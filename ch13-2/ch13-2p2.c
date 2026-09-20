da0a9a5032d0fcff24ccbc6c1a25071598c2dc90
HDLee26 <lhd2600160@kunsan.ac.kr>
2026-09-20 14:56:24 +0900

    Create ch13-2p2.c

---
+++ ch13-2/ch13-2p2.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS // 보안오류방지
+#pragma warning(disable:6031)  // 리턴값관련 경고 방지
+
+#include <stdio.h>
+
+int main(void)
+{
+	char s[50] = { 0 };
+	int i, cnt;
+
+	printf("문자열을 입력하시오: ");
+	scanf("%s", s);
+
+	// 문자열 길이 구하기
+	i = 0, cnt = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+		cnt++;
+	}
+	
+	// 문자 각각 출력
+	for (int i = 0; i < cnt; i++)
+	{
+		printf("%d번재 문자 %c\n", i+1, s[i]);
+	}
+
+	return 0;
+
+}
